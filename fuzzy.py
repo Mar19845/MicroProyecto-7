f9963c58c33acf02d383c00524ef548e431e96bb
Mar19845 <mar19845@uvg.edu.gt>
2022-10-20 23:53:24 -0600

    fuzzy logic (?)

---
+++ fuzzy.py
@@ -0,0 +1,77 @@
+from helpers import *
+import math
+
+
+actions = [
+    # first position is movement and second rotation
+    [1, 0], [1, math.pi / 6.0], [1, math.pi / 3.0], #slow
+    [5, 0], [5, math.pi / 6.0], [5, math.pi / 3.0], # normal
+    [10, 0], [10, math.pi / 6.0], [10, math.pi / 3.0] # fast
+]
+class Fuzzy():
+    # Membership functions for distance
+    def distance_close(x):
+        if x <= 2:
+            return 1.0
+        elif x > SCREEN_DIMENSION/2.0:
+            return 0.0
+        return -0.197197430123091 * x + 1.39439486024618
+    
+    def distance_medium(x):
+        if x <= SCREEN_DIMENSION/2.0:
+            return 0.141421512474792 * x
+        return -0.141421312474633 * x + 1.99999858578688
+    
+    def distance_far(x):
+        if x <= SCREEN_DIMENSION/2.0:
+            return 0.0
+        elif x > SCREEN_DIMENSION:
+            return 1.0
+        return 0.341420445621966 * x - 2.41420445621966
+    
+    def angle_close(x):
+        if x <= math.pi / 6.0:
+            return 1.0
+        elif x >= math.pi / 2.0:
+            return 0.0
+        return -0.954929658365891 * x + 1.49999999990451
+    
+    def angle_medium(x):
+        if x <= math.pi / 2.0:
+            return 0.636619772284456 * x
+        return -0.636619772284456 * x + 2
+    
+    def angle_far(x):
+        if x <= math.pi / 2.0:
+            return 0.0
+        elif x >= 3.0 * math.pi / 2.0:
+            return 1.0
+        return 0.318309886243549 * x - 0.500000000159155
+    
+    def loop(ball,player):
+        # get the distance between  the player and the ball
+        distance = player.position.distance(ball.position)
+        direction_2_ball = player.position.direction_to(ball.position)
+        angle = player.direction.angle(direction_2_ball)
+        
+        distance_f = [Fuzzy.distance_close(distance), Fuzzy.distance_medium(distance), Fuzzy.distance_far(distance)]
+        rotation_f = [Fuzzy.angle_close(angle), Fuzzy.angle_medium(angle), Fuzzy.angle_far(angle)]
+        
+        # add min value to rules list
+        rules = []
+        for dist in distance_f:
+            for rot in rotation_f:
+                rules.append(min(dist, rot))
+        
+        max_value = -1
+        index = 0     
+        for i in range(len(rules)):
+            if rules[i] > max_value:
+                max_value = rules[i]
+                index = i
+        
+        # get action      
+        action = actions[index]
+        player.rotate(rad=action[1], clockwise=(player.position.y >= ball.position.y))
+        player.move(speed=action[0])
+        return distance, angle
---
+++ main.py
@@ -0,0 +1,107 @@
+import random
+import math
+import pygame
+from pygame.locals import(KEYDOWN,K_ESCAPE,QUIT)
+from helpers import *
+from fuzzy import Fuzzy
+from ObjectGui import ModelObject
+
+
+def main():
+    
+    # randomn seed
+    random.seed(random_seed)
+    pygame.init()
+    screen = pygame.display.set_mode([SCREEN_DIMENSION, SCREEN_DIMENSION])
+    background = pygame.image.load("utils/cancha.png").convert()
+    ball = ModelObject(sprite_path="utils/ball.png")
+    ball.rotation = 0.0
+
+    player = ModelObject(sprite_path="utils/messi.png")
+
+    goal = pygame.Surface((GOAL_WIDTH, GOAL_HEIGHT))
+    goal.fill(BLACK)
+
+    message_font = pygame.font.Font('ItalianFootball.ttf', 30)
+    over_text = message_font.render('GOOOOOL' + '\n '+ 'PRESS R TO RESET', False, WHITE)
+
+    textRect = over_text.get_rect()
+    textRect.center = (SCREEN_DIMENSION / 2, SCREEN_DIMENSION / 2 - 150)
+
+    # game state
+    game_running = True
+    ball_shot = False
+    robot_won = False
+
+
+    while game_running:
+        # quit event, click or key
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_running = False
+            elif event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    game_running = False
+
+        screen.blit(background, (0, 0))
+        
+        # fuzzy loop
+        if not ball_shot:
+            dist, angle = Fuzzy.loop(ball, player)
+            
+        # the player reaches the ball
+        if not ball_shot and dist <= MIN_DIST_2_BALL:
+            dist_font = pygame.font.SysFont(None, 30)
+            dist_surf = dist_font.render('Robot reached the ball!', True, (BLACK))
+            screen.blit(dist_surf, (5, 30))
+            
+            # direction to goal
+            rnd_rot_dev = math.pi / 6.0 * random.random()
+            if random.random() <= 0.5:
+                rnd_rot_dev *= -1.0
+            ball.set_direction(rad=(math.pi / 2.0 + rnd_rot_dev))
+            ball.speed = SPEED
+            ball_shot = True
+        
+        # ball bounces on wall
+        if ball.position.x < 70 or ball.position.x > SCREEN_DIMENSION-70:
+            ball.rotate(math.pi)
+        if ball.position.y < 70 or ball.position.y > SCREEN_DIMENSION-70:
+            ball.rotate(math.pi)
+            
+        # reduce ball speed and stop ball if necessary
+        if ball_shot:
+            ball.speed -= FRICTION
+            ball.move(ball.speed)
+            if ball.speed < 0.1:
+                ball.speed = 0.0
+                ball_shot = False
+                
+        # ball reaches goal
+        # simple detection if its on the range
+        if ball.position.y > GOAL_POSITION:
+            if ball.position.x > (SCREEN_DIMENSION / 2 - GOAL_WIDTH / 2) and ball.position.x < ((SCREEN_DIMENSION / 2 - GOAL_WIDTH / 2) + GOAL_WIDTH):
+                screen.blit(over_text, textRect)
+                robot_won = True
+
+        # draw game objects
+        screen.blit(ball.surf, (ball.position.x, ball.position.y))
+        screen.blit(player.surf, (player.position.x, player.position.y))
+        screen.blit(goal, (SCREEN_DIMENSION / 2 - GOAL_WIDTH / 2, SCREEN_DIMENSION - GOAL_HEIGHT))
+        
+        # debug messages
+        message_surf = message_font.render(f'Distance to ball: {dist:.{2}f}', True, (BLACK))
+        screen.blit(message_surf, (25, 25))
+
+        pygame.display.flip()
+
+        while robot_won:
+            # quit event, click or key
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                elif event.type == KEYDOWN:
+                    if event.key == pygame.K_r:
+                        main()
+if __name__ == '__main__':
+    main()
